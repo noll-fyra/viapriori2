e5e08fbf8158d9a4779259e12a4c480493fbc542
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-19 15:54:57 +0800

    fix fb hosting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-import db from './firebaseThing'
-
-db.ref().on('value', snapshot => {
   ReactDOM.render(
     <App />,
     document.getElementById('root')
   )
-})
