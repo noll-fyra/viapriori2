a2886690a1fff39d7912d71e71ef078a99a4aab0
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-23 15:22:20 +0800

    add exif

--- src/components/new/New.js
+++ src/components/new/New.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import db, {auth, storage} from '../../utils/firebase'
+import EXIF from 'exif-js'
+var ExifImage = require('exif').ExifImage
 
 class New extends React.Component {
   constructor (props) {
@@ -10,7 +12,8 @@ class New extends React.Component {
       imageName: 'Add an image',
       imageLocation: '',
       imageDate: '',
-      description: ''
+      description: '',
+      image: ''
     }
     this.addedTitle = this.addedTitle.bind(this)
     this.addedStart = this.addedStart.bind(this)
@@ -39,10 +42,44 @@ class New extends React.Component {
   }
 
   addedFile (e) {
+    var reader = new window.FileReader()
+    reader.addEventListener('load', () => {
+      this.setState({
+      //   // imagePath: e.target.files[0],
+      //   // imageName: e.target.files[0].name,
+        image: reader.result
+      })
+
+
+    })
+    reader.readAsDataURL(e.target.files[0])
+
+    // var x = reader.readAsDataURL(e.target.files[0])
     this.setState({
       imagePath: e.target.files[0],
       imageName: e.target.files[0].name
     })
+    let image = e.target.files[0]
+  EXIF.getData(image, () => {
+    var date = image.exifdata.DateTime
+    console.log(image.exifdata);
+    });
+
+    // EXIF.getData(this.state.image, () => {
+    //   var x = EXIF.getTag(this.state.image, 'Model')
+    //   console.log(x)
+    // })
+
+      // new ExifImage({image: 'image.jpg'}, (err,data) => {
+      //   if(err) {
+      //     console.log(err.message);
+      //     console.log('hi');
+      //   } else {
+      //     console.log(data);
+      //     console.log('bye');
+      //   }
+      // })
+
   }
 
   addedDescription (e) {
@@ -68,19 +105,30 @@ class New extends React.Component {
     return (
       <div>
         <div className='backdrop' onClick={this.props.onClose} />
+
+        {this.state.imagePath === '' &&
         <div className='modal'>
           <label>
             <span>{this.state.imageName}</span>
             <input className='fileInput' type='file' onChange={(e) => this.addedFile(e)} />
           </label>
         </div>
+        }
+
         {this.state.imagePath !== '' &&
-        <div className='modal modal2'>
-          <p><input type='text' onChange={(e) => this.addedTitle(e)} placeholder='Title' /></p>
-          <p><input type='date' onChange={(e) => this.addedStart(e)} /></p>
-          <p><input type='date' onChange={(e) => this.addedEnd(e)} /></p>
-          <p><textarea onChange={(e) => this.addedDescription(e)} /></p>
-          <button onClick={this.addActivity}>Post</button>
+        <div>
+
+          <div className='modal' style={{backgroundImage: `url(${this.state.image})`, backgroundSize: 'cover'}}>
+            {/* <img src={this.state.image} alt='' /> */}
+
+          </div>
+          <div className='modal modal2'>
+            <p><input type='text' onChange={(e) => this.addedTitle(e)} placeholder='Title' /></p>
+            <p><input type='date' onChange={(e) => this.addedStart(e)} /></p>
+            <p><input type='date' onChange={(e) => this.addedEnd(e)} /></p>
+            <p><textarea onChange={(e) => this.addedDescription(e)} /></p>
+            <button onClick={this.addActivity}>Post</button>
+          </div>
         </div>
         }
       </div>
