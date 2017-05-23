42aa143bc9277885ced0342340f379b5090dd05e
Jerel Lim <j.limster@gmail.com>
2017-05-23 19:59:55 +0800

    added profile pic

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -11,59 +11,73 @@ class Profile extends React.Component {
     this.state = {
       user: '',
       searchQuery: '',
-      imageURL: "Add Profile Picture"
+      imagePath: '',
+      imageName: 'Add Profile Picture',
+      image: ''
     }
   }
-  // componentDidMount () {
-  //   db.ref('users/' + auth.currentUser.uid).on('value', (snapshot) => {
-  //     console.log(snapshot)
-  //     console.log(snapshot.val())
-  //     this.setState({
-  //       username: auth.currentUser.username,
-  //       email: auth.currentUser.email,
-  //     })
-  //   })
-  // }
-  addProfilePic(e) {
-console.log(e.target.files[0])
-console.log(e.target)
-console.log(e.target.value)
-console.log(e.target.files[0].name)
-    db.ref('users/' + auth.currentUser.uid+ '/details').update({
-      profileURL: e.target.value
-      // dob: null,
-      // tripsCompleted: [],
-      // tripsSaved: [],
-      // tripsFavourited: [],
-      // following: [],
-      // followedBy: []
-    })
-    this.setState({
-      profileURL: e.target.value
-
+  componentDidMount () {
+    db.ref('users/' + auth.currentUser.uid).on('value', (snapshot) => {
+      this.setState({
+        username: snapshot.val().details.username,
+        email: snapshot.val().details.email,
+      })
     })
   }
+  addProfilePic(e) {
+
+    let image = e.target.files[0]
+    console.log(image)
+    var reader = new window.FileReader()
+    reader.addEventListener('load', () => {
+      this.setState({
+        image: reader.result
+      })
+    })
+    reader.readAsDataURL(image)
+
+    this.setState({
+      imagePath: image,
+      imageName: image.name
+    })
+
+    // db.ref('users/' + auth.currentUser.uid+ '/details').update({
+      // profileURL: image
+    // })
+  }
   render () {
-    // const trips = this.state.tripDisplayed.filter((trip) => { return trip.includes(this.state.searchQuery) })
+
     return (
       <div>
-
         <h1> User Profile</h1>
 
+        {this.state.imagePath === '' &&
         <div className='profileDiv'>
-          <label className = 'profile'>
-            <span>{this.state.imageURL}</span>
+          <label className='profile'>
+            <span>{this.state.imageName}</span>
             <input className='fileInput' type='file' onChange={(e) => this.addProfilePic(e)} />
           </label>
         </div>
+        }
+
+        {this.state.imagePath !== '' &&
+        <div className = 'profileDiv'>
+
+          <label className='profile' style={{backgroundImage: `url(${this.state.image})`, backgroundSize: 'cover'}}>
+            <span className='editImage'>Edit Image</span>
+            <input className='fileInput' type='file' onChange={(e) => this.addProfilePic(e)} />
+          </label>
+        </div>
+}
 
 
-        {/* <h2> Name: {auth.currentUser.username}</h2>
-        <h2> email: {auth.currentUser.email}</h2> */}
-
-        {/* <ProfileDetails profileDetails={profile} /> */}
-        {/* <TripOverview tripItems={trips} /> */}
+<h4> Name: {this.state.username}</h4>
+<h4> email: {auth.currentUser.email}</h4>
       </div>
+       //
+      //
+      //   <ProfileDetails profileDetails={profile} />
+      //  <TripOverview tripItems={trips} />
     )
   }
 }
