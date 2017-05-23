ce9fc168bf6f5617901b6bf586b57eb7ee9089c8
Jerel Lim <j.limster@gmail.com>
2017-05-23 18:37:04 +0800

    profile pic css

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import db, {auth} from '../../utils/firebase'
+import db, {auth} from '../../utils/firebase'
 // import search from '../../utils/search'
 // import SearchForm from '../search/SearchForm'
 // import TripOverview from '../trip/TripOverview'
@@ -9,37 +9,58 @@ class Profile extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      userDisplayed: 'asd',
-      tripDisplayed: ['america', 'russia', 'iceland'],
-      searchQuery: ''
+      user: '',
+      searchQuery: '',
+      imageURL: "Add Profile Picture"
     }
-    // this.search = search.bind(this)
   }
   // componentDidMount () {
-  //   db.ref('users').on('value', (snapshot) => {
-  //     const keys = []
-  //     const allUsers = []
-  //     for (var key in snapshot.val()) {
-  //       keys.push(key)
-  //       let tripEnd = new Date(snapshot.val()[key].end)
-  //       let tripStart = new Date(snapshot.val()[key].start)
-  //       let tripDuration = (tripEnd - tripStart) / 86400000
-  //       let text = tripDuration + ' days: ' + snapshot.val()[key].title
-  //       allTrips.push(text.toString())
-  //     }
+  //   db.ref('users/' + auth.currentUser.uid).on('value', (snapshot) => {
+  //     console.log(snapshot)
+  //     console.log(snapshot.val())
   //     this.setState({
-  //       database: snapshot.val(),
-  //       tripId: keys,
-  //       tripDisplayed: allTrips
+  //       username: auth.currentUser.username,
+  //       email: auth.currentUser.email,
   //     })
-  //     console.log(this.state.tripDisplayed)
   //   })
   // }
+  addProfilePic(e) {
+console.log(e.target.files[0])
+console.log(e.target)
+console.log(e.target.value)
+console.log(e.target.files[0].name)
+    db.ref('users/' + auth.currentUser.uid+ '/details').update({
+      profileURL: e.target.value
+      // dob: null,
+      // tripsCompleted: [],
+      // tripsSaved: [],
+      // tripsFavourited: [],
+      // following: [],
+      // followedBy: []
+    })
+    this.setState({
+      profileURL: e.target.value
+
+    })
+  }
   render () {
     // const trips = this.state.tripDisplayed.filter((trip) => { return trip.includes(this.state.searchQuery) })
     return (
       <div>
+
         <h1> User Profile</h1>
+
+        <div className='profileDiv'>
+          <label className = 'profile'>
+            <span>{this.state.imageURL}</span>
+            <input className='fileInput' type='file' onChange={(e) => this.addProfilePic(e)} />
+          </label>
+        </div>
+
+
+        {/* <h2> Name: {auth.currentUser.username}</h2>
+        <h2> email: {auth.currentUser.email}</h2> */}
+
         {/* <ProfileDetails profileDetails={profile} /> */}
         {/* <TripOverview tripItems={trips} /> */}
       </div>
