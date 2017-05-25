4654592fbb2bcf00ae4951d947caef2ade583a6b
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-05-25 13:17:52 +0800

    fix trips

--- src/components/trip/MyTrips.js
+++ src/components/trip/MyTrips.js
@@ -17,38 +17,60 @@ class MyTrips extends React.Component {
       this.setState({
         keys: Object.keys(snapshot.val())
       })
+      let temp = []
+      let images = []
       for (var key in snapshot.val()) {
-        db.ref('trips/' + key).once('value', (snapshot) => {
+        db.ref('trips/' + key).once('value', (snap) => {
+          // this.setState({
+          //   trips: this.state.trips.concat(snap.val())
+          // })
+          temp.push(snap.val())
           this.setState({
-            trips: this.state.trips.concat(snapshot.val())
+            trips: temp
           })
-          if (snapshot.val().image) {
-            storage.ref(snapshot.val().image).getDownloadURL().then((url) => {
+          if (snap.val().image) {
+            storage.ref(snap.val().image).getDownloadURL().then((url) => {
+              images.push(url)
               this.setState({
-                images: this.state.images.concat(url)
+                images: images
               })
             })
           } else {
+            images.push('')
             this.setState({
-              images: this.state.images.concat('')
+              images: images
             })
+            // this.setState({
+            //   images: this.state.images.concat('')
+            // })
           }
+          // this.setState({
+          //   trips: temp,
+          //   images: images
+          // })
         })
+        // this.setState({
+        //   trips: temp
+        // })
       }
+
+
     })
   }
 
   render () {
-    const allTrips = this.state.trips.map((trip, index) => {
-      return <TripOverview key={this.state.keys[index]} tripID={this.state.keys[index]} trip={trip} image={this.state.images[index]} />
-    })
+    // const allTrips = this.state.trips.map((trip, index) => {
+    //   return <TripOverview key={this.state.keys[index]} tripID={this.state.keys[index]} trip={trip} image={this.state.images[index]} />
+    // })
 
     return (
       <div className='trips'>
-        {JSON.stringify(this.state.keys)}
-        {JSON.stringify(this.state.trips)}
-        {JSON.stringify(this.state.images)}
-        {allTrips}
+        <p>keys{JSON.stringify(this.state.keys)}</p>
+        <p>trips{JSON.stringify(this.state.trips)}</p>
+        <p>images{JSON.stringify(this.state.images)}</p>
+        {this.state.trips.map((trip, index) => {
+          return <TripOverview key={this.state.keys[index]} tripID={this.state.keys[index]} trip={trip} image={this.state.images[index]} />
+        })}
       </div>
     )
   }
