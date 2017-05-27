360a0036be3d5ded8280e3a9d4251debf98ca833
Jerel Lim <j.limster@gmail.com>
2017-05-27 14:42:54 +0800

    adding planned trips section

--- src/components/saved/Saved.js
+++ src/components/saved/Saved.js
@@ -3,18 +3,21 @@ import db, {storage, storageKey} from '../../utils/firebase'
 import SavedOverview from './SavedOverview'
 import Planned from '../planned/Planned'
 
-
 class Saved extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      savedArray:[],
-      savedActivities:[],
+      savedArray: [],
+      savedActivities: [],
+      plannedTrips: [],
+      isNewPlanned: false
     }
+    // this.addTripTitle = this.addTripTitle.bind(this)
   }
   componentDidMount () {
     db.ref('users/' + window.localStorage[storageKey] + '/saved').once('value').then((snapshot) => {
       this.displayActivity()
+      this.currentPlannedTrips()
     })
   }
   displayActivity () {
@@ -49,31 +52,116 @@ class Saved extends React.Component {
     this.displayActivity()
   }
 
+// ////////// planned trip methods from here down
 
-  plannedActivity(e){
-    let savedActivityID = e.target.name
-    db.ref('users/' + window.localStorage[storageKey] + '/saved').once('value').then((snap) => {
-      let newObj = snap.val() || {}
-      newObj[savedActivityID] = true
-      db.ref('users/' + window.localStorage[storageKey] + '/saved').set(newObj)
+  // plannedActivity(e){
+  //   let savedActivityID = e.target.name
+  //   db.ref('users/' + window.localStorage[storageKey] + '/saved').once('value').then((snap) => {
+  //     let newObj = snap.val() || {}
+  //     newObj[savedActivityID] = true
+  //     db.ref('users/' + window.localStorage[storageKey] + '/saved').set(newObj)
+  //   })
+  // }
+
+  startNewPlanned (boolean) {
+    this.setState({
+      isNewPlanned: boolean
     })
   }
 
-  startNewPlanned(boolean){
-      this.setState({
-        isNewPlanned: boolean
-      })
+  currentPlannedTrips () {
+    db.ref('users/' + window.localStorage[storageKey] + '/planned').once('value').then((snapshot) => {
+      if (snapshot.val()) {
+        let plannedArray = Object.keys(snapshot.val())
+        this.setState({
+          plannedArray: plannedArray
+        })
+        if (plannedArray.length === 0) {
+          this.setState({
+            isNewPlanned: true
+          })
+        }
+        let tempPlanned = plannedArray.slice()
+        for (var trip in snapshot.val()) {
+          let index = plannedArray.indexOf(trip)
+          db.ref('trips/' + trip).once('value').then((snap) => {
+            tempPlanned[index] = snap.val()
+            this.setState({
+              plannedTrips: tempPlanned
+            })
+          })
+        }
+      } else {
+        this.setState({
+          plannedTrips: []
+        })
+      }
+    })
   }
+  //
+  // addPlannedTripTitle (e) {
+  //
+  //   this.setState({
+  //     newPlannedTripName: e.target.value
+  //   })
+  // }
+  createNewPlanned (e) {
+    e.preventDefault()
+    let newPlannedTripID = ''
+    if (this.state.isNewPlanned) {
+      const form = e.target
+      const input = form.querySelector('#newPlanned')
+        // console.log(input.value)
+      this.setState({
+        newPlannedTripName: input.value
+      })
+      let plannedtrips = db.ref('plannedtrips')
+      let newRef = plannedtrips.push()
+      let newPlannedTripID = newRef.key
+      newRef.set({
+        user: window.localStorage[storageKey],
+        title: this.state.newPlannedTripName
+      })
+      db.ref('users/' + window.localStorage[storageKey] + '/planned').once('value', snap => {
+        let newObj = snap.val() || {}
+        newObj[newPlannedTripID] = true
+        db.ref('users/' + window.localStorage[storageKey] + '/planned').set(newObj)
+      })
+    }
+      let plannedTripID = this.state.isNewPlanned ? newPlannedTripID : this.state.plannedArray.reverse()[this.state.tripIndex]
+  }
+  chooseTrip (e) {
+    this.setState({
+      tripIndex: e.target.selectedIndex
+    })
+  }
+
   render () {
-      let reverseSaved = this.state.savedActivities.slice().reverse()
+    let reverseSaved = this.state.savedActivities.slice().reverse()
+    let plannedArray = this.state.plannedTrips.slice().reverse()
+    let options = plannedArray.map((title, index) => {
+      return <option key={index}>{title}</option>
+    })
+    console.log(this.state.isNewPlanned)
     return (
       <div>
-        <Planned/>
+        {/* <Planned/> */}
         <h1> Saved Activities</h1>
 
         {this.state.savedActivities &&
             reverseSaved.map((activity, index) => {
-              return <SavedOverview key={index} activityID={this.state.savedArray[index]} activity={activity} handlePlannedActivity={(e) => this.plannedActivity(e)} handleRemoveSaved={(e) => this.removeActivity(e)} isplanned={this.state.isNewPlanned}/>
+              return <SavedOverview key={index}
+                activityID={this.state.savedArray[index]}
+                activity={activity}
+              // handlePlannedActivity={(e) => this.plannedActivity(e)}
+                handleRemoveSaved={(e) => this.removeActivity(e)}
+                isPlanned={this.state.isNewPlanned}
+                options={options}
+                chooseTrip={(e) => this.chooseTrip(e)}
+                trips={plannedArray}
+                startNewPlanned={(e) => this.startNewPlanned(true)}
+                switchNewPlanned={(e) => this.startNewPlanned(false)}
+                createNewPlanned={(e) => this.createNewPlanned(e)} />
             })}
       </div>
     )
--- src/components/saved/SavedOverview.js
+++ src/components/saved/SavedOverview.js
@@ -10,25 +10,40 @@ const SavedOverview = (props) => {
       <div className='activityOverview'><p>Activity: {props.activity.title||''}</p><img src={props.activity.image} alt={props.activity.title}/>
         <p>Date: {moment(props.activity.date).format('YYYY-MM-DD')|| ''}</p><p>City:{props.activity.city||''}</p><p>Country: {props.activity.country||''}</p><p>Caption:{props.activity.caption||''}</p><Rating stars={props.activity.rating} isEnabled={true}/>
 
-      
+        {props.trips.length === 0 &&
+            <form onSubmit={props.createNewPlanned} name={props.activityID}>
+              <input id='newPlanned' type='text' placeholder='Add title for your first planned trip!'/>
+              <input type='submit' value="Create Planned Trip" />
+            </form>
+        }
 
-        {!props.isPlannedTrip && this.state.trips.length > 0 &&
-        <p>
+        {!props.isPlanned && props.trips.length > 0 &&
+        <div>
+          <form onSubmit={props.createNewPlanned} name={props.activityID}>
           <label>
             Add to:
-            <select onChange={this.chooseTrip}>
-              {options}
+            <select onChange={props.chooseTrip}>
+              {props.options}
             </select>
           </label>
-           or <button onClick={() => this.startNewPlanned(true)}>Plan a trip</button>
-        </p>
-        }
-        {props.isPlannedTrip && this.state.trips.length > 0 &&
-          <p><input type='text' name={props.activityID} onChange={props.handlePlannedActivity} placeholder='Add new trip' />
-        or <button name={props.activityID} onClick={() => this.startNewPlanned(false)}>Add to existing plans</button></p>
+            <input type='submit' value="Add activity" />
+          </form>
+           or <button onClick={props.startNewPlanned}>Plan a new trip</button>
+        </div>
         }
 
-        <button name={props.activityID} onClick={props.handleRemoveSaved}>Remove</button>
+        {!props.isPlanned && props.trips.length > 0 &&
+          <div>
+        <form onSubmit={props.createNewPlanned} name={props.activityID}>
+          <input id='newPlanned' type='text' placeholder='Add title for your first planned trip!'/>
+          <input type='submit' value="Create Planned Trip" />
+        </form>
+                or
+          <button name={props.activityID} onClick={props.switchNewPlanned}>Add to existing plans</button>
+          <button name={props.activityID} onClick={props.handleRemoveSaved}>Remove</button>
+          </div>
+        }
+
 
       </div>
 
