1b6d4c79f6ccb1a1bdba0afd48c6a101b1aa0a17
Jerel Lim <j.limster@gmail.com>
2017-06-01 12:14:22 +0800

    before css merge

--- src/components/planned/PlannedOverview.js
+++ src/components/planned/PlannedOverview.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './planned.css'
+
 // import db, {storageKey} from '../../utils/firebase'
 // onClick={() => props.changeTripId()}
 const PlannedOverview = (props) => {
   return (
     <div>
       <Link to={'/planned/' + props.tripID}>
-        <div className='tripOverview'>
-          <p>{props.trip.title || ''} </p>
+        <div className='plannedOverview'>
+          <p>{props.trip.title || ''}</p>
 
           {/* <img src={props.trip.image} alt={props.trip.title || ''} /> */}
         </div>
